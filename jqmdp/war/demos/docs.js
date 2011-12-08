11501376ad3a4de658f0d44b71da56a921e89ea6
kotemaru@kotemaru.org <kotemaru@kotemaru.org@1e2d0e59-f8b1-42fb-e57c-f9934030a7fa>
2011-12-08 05:52:05 +0000

    IE 100%

--- jqmdp/war/demos/docs.js
+++ jqmdp/war/demos/docs.js
@@ -7,7 +7,7 @@ Docs.trimHtml = function(str) {
 		.replace(/<head>([\r\n]|.)*<\/head>/,"<head>...</head>")
 		.replace(/&/g,"&amp;")
 		.replace(/</g,"&lt;")
-		.replace(/(data-dp-[a-z]+=".*")/g,"<b>$1</b>")
+		.replace(/(data-dp-[a-z]+="[^"]*")/g,"<b>$1</b>")
 		.replace(/(on[a-z]+="[^"]*[$][.]jqmdp[.(][^"]*")/g,"<b>$1</b>")
 		.replace(/(on[a-z]+="[^"]*[$][.]scope[.(][^"]*")/g,"<b>$1</b>")
 		.replace(/&lt;!--(.*)-->/g,"&lt;!--<i>$1</i>-->")
